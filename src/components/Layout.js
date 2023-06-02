2287b9aeb73c5033db540ea57471ba055e9eec0b
carlos.medina <carlos.mdn94@gmail.com>
2023-06-01 23:18:17 -0700

    session 1

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import { Container } from "@mui/material";
+import Header from "./Header";
+import Footer from "./Footer";
+import Link from "next/link";
+
+export default function Layout ({children}) {
+    return (
+        <>
+        <Header title="Carlos Medina">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/projects">Projects</Link>
+        </Header>
+        <Container fixed>
+            <main>{children}</main>
+        </Container>
+        <Footer/>
+        </>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
-export default function Home () {
-  return <h1>Home Page</h1>
+import { useRouter } from "next/router";
+import { Button } from "@mui/material";
+import styles from "../styles/Home.module.css";
+
+export default function Home ({name, summary}) {
+
+  const router = useRouter();
+  return (
+    <section className={styles.Home}>
+      <h1 className={styles.Name}>{name}</h1>
+      <div className={styles.Summary}>{summary}</div>
+      <div>
+        <Button variant="contained" size="large" onClick={() => router.push("/projects")}>
+          Projects
+        </Button>
+      </div>
+    </section>
+  );
+}
+
+export async function getStaticProps () {
+  // Get you date from an API
+  console.log("hola");
+  return {
+    props: {
+      name: "Carlos Medina",
+      summary:
+        "I am a skilled full-stack developer with expertise in both front-end and back-end technologies. I specialize in designing and implementing innovative solutions and strive to create user-friendly interfaces and high-quality web applications. I am passionate about leveraging technology to solve complex problems and continuously expanding my skills in the dynamic field of full-stack development."
+    }
+  }
 }
